9672b2f2cfb59b819cdc71d32563921eefc16d02
Tauseef Shaikh <48127782+tauseef-dev@users.noreply.github.com>
2020-11-23 16:13:44 +0530

    Add files via upload

---
+++ exp08/fetch_item.php
@@ -0,0 +1,35 @@
+<?php
+
+//fetch_item.php
+
+include('database_connection.php');
+
+$query = "SELECT * FROM tbl_product ORDER BY id DESC";
+
+$statement = $connect->prepare($query);
+
+if($statement->execute())
+{
+	$result = $statement->fetchAll();
+	$output = '';
+	foreach($result as $row)
+	{
+		$output .= '
+		<div class="col-md-3" style="margin-top:20px;">  
+            <div style="border:0px solid #333; background-color:ffffff; border-radius:5px; padding:16px; height:350px;" align="center">
+            	<img src="img/'.$row["image"].'" class="img-responsive" /><br />
+            	<h3 class="text-info">'.$row["name"].'</h3>
+            	<h4 class="text-danger">₹ '.$row["price"] .'</h4>
+            	<input type="text" name="quantity" id="quantity' . $row["id"] .'" class="form-control" value="1" />
+            	<input type="hidden" name="hidden_name" id="name'.$row["id"].'" value="'.$row["name"].'" />
+            	<input type="hidden" name="hidden_price" id="price'.$row["id"].'" value="'.$row["price"].'" />
+            	<input type="button" name="add_to_cart" id="'.$row["id"].'" style="margin-top:10px;" class="btn btn-success form-control add_to_cart" value="Add to Cart" />
+            </div>
+        </div>
+		';
+	}
+	echo $output;
+}
+
+
+?>
